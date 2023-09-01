108baa9d1db868bae9480e7bbeb27b75bf5108d4
YsisC <ysisclongart@gmail.com>
2023-09-01 14:09:48 -0400

    config

--- next.config.js
+++ next.config.js
@@ -2,17 +2,6 @@
 const nextConfig = {
     reactStrictMode: true,
   
-   // Add basePath
-    basePath: '/github-pages',
-    images: {
-      loader: 'imgix', // Puedes usar otros loaders si lo prefieres
-      path: 'https://YsisC.github.io/calculadora-redux/', // Reemplaza con la URL de tu CDN
-    },
-    outDir: './build',
-    experimental: {
-      images: true,
-    },
-  
   }
   
   module.exports = nextConfig
